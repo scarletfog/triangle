8abfca14583f2fa581cd3777d0c2c38d06287afb
scarletfog <scarletfog@gmail.com>
2016-09-25 16:38:05 +0200

    triangle

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a = prompt('Podaj długość podstawy w cm');
+var h = prompt('Podaj wysokośc w cm');
+var triangleArea = a*h/2;
+
+alert('Pole trójkąta o podstawie a: ' + a + 'cm' + ' i wysokości h: ' + h + 'cm' + ' wynosi: ' + triangleArea + 'cm kw');
+
+console.log(triangleArea);
